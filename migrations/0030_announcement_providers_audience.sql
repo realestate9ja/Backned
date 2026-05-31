92d8661516a5d9f1913d13570e1d37ac66b9ad89
Dave <Davidgeorgechigoziri@gmail.com>
2026-05-31 15:24:15 +0100

    fix

--- migrations/0030_announcement_providers_audience.sql
+++ migrations/0030_announcement_providers_audience.sql
@@ -1,18 +1,5 @@
-DO $$
-DECLARE
-    constraint_name text;
-BEGIN
-    SELECT conname
-    INTO constraint_name
-    FROM pg_constraint
-    WHERE conrelid = 'announcements'::regclass
-      AND contype = 'c'
-      AND pg_get_constraintdef(oid) LIKE '%audience IN%';
-
-    IF constraint_name IS NOT NULL THEN
-        EXECUTE format('ALTER TABLE announcements DROP CONSTRAINT %I', constraint_name);
-    END IF;
-END $$;
+ALTER TABLE announcements
+    DROP CONSTRAINT IF EXISTS announcements_audience_check;
 
 ALTER TABLE announcements
     ADD CONSTRAINT announcements_audience_check
